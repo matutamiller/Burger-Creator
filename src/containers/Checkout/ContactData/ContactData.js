f3cba035fdea0ca2013151990bdd34661f5f609b
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-09-02 15:54:46 +0200

    Working on an Error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,6 +84,7 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
+                validation: {},
                 value: ''
             }
         },
@@ -115,6 +116,10 @@ class ContactData extends Component {
 
     checkValidity (value, rules) {
         let isValid = true;
+
+        if(!rules) {
+            return true;
+        }
 
         if(rules.required) {
             isValid = value.trim() !== '' && isValid;
