41242f46595dc29ecaed33a4334656ff5307ec10
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-10-07 13:41:23 +0200

    Connecting Contact Data Container & Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,8 +6,9 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 
 import classes from './ContactData.css'
 import axios from '../../../axios-orders';
-
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import Input from '../../../components/UI/Input/Input';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
     state = {
@@ -96,8 +97,6 @@ class ContactData extends Component {
 
     orderHandler = (event) => {
         event.preventDefault();
-        
-        this.setState({loading: true});
         const formData = {};
         for (let formElementIdentifier in this.state.orderForm) {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
@@ -107,7 +106,7 @@ class ContactData extends Component {
             price: this.props.price,
             orderData: formData  
         }
-        
+        this.props.onOrderBurger(order);
     }
 
     checkValidity (value, rules) {
@@ -197,4 +196,8 @@ const mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+};
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
