a90454dc5ad137358e53c4f4553ffdfc3669661b
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-11-09 06:11:21 +0100

    Adding Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
 import { Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
-import Checkout from './containers/Checkout/Checkout'
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
+
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+
+const asycCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asycOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
+
+const asycAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
 
 class App extends Component { 
   
@@ -20,7 +29,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth}/>
+        <Route path="/auth" component={asycAuth}/>
         <Route path="/" exact component={BurgerBuilder}/> 
         <Redirect to="/"/>
       </Switch>
@@ -30,10 +39,10 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-           <Route path="/checkout" component={Checkout}/>
-          <Route path="/orders" component={Orders}/>
+           <Route path="/checkout" component={asycCheckout}/>
+          <Route path="/orders" component={asycOrders}/>
           <Route path="/logout" component={Logout}/>
-          <Route path="/auth" component={Auth}/>
+          <Route path="/auth" component={asycAuth}/>
           <Route path="/" exact component={BurgerBuilder}/> 
           <Redirect to="/"/>
         </Switch>
