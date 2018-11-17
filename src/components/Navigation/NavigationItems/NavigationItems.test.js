a84f99b7b7db5f0329e05a54a6c2c26de4a8bff6
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-11-17 18:54:43 +0100

    Using Different Methods for Tests

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -17,9 +17,15 @@ describe('<NavigationItems />', () => {
     it('should render two <NavigationItem /> elements if not authenticated', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
+
     it('should render tree <NavigationItem /> elements if authenticated', () => {
         //wrapper = shallow(<NavigationItems isAuthenticated/>);
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render tree <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
 });
