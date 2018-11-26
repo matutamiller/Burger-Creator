b9be3efe27cc247d791d1e7a4c63c039f972dcce
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-11-26 16:24:31 +0100

    Moving More Logic Into Sagas

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -31,12 +31,22 @@ export const logout = () => {
     };
 };
 
-export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000);
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
     };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return {
+        type: actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime: expirationTime
+    }
+    //return dispatch => {
+    //    setTimeout(() => {
+    //        dispatch(logout());
+    //    }, expirationTime * 1000);
+    //};
 };
 
 export const auth = (email, password, isSignup) => {
